ab31ffe2b47e755b027dc988d7b2e93aaa14593d
Harshit Mishra <165307122+1harshit234@users.noreply.github.com>
2024-05-09 07:47:10 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -6,7 +6,7 @@ y = float(input("write the number you want2"))
 print("1 = add,2 = subtract,3 = multiply,4= division,5 = power, 6 = sqrt ")
 #taking the operation which the user wanna do it
 z = int(input("write the operation u want to do with the numbers you want "))
-#condition for differetn different type of the operations
+#condition for different different type of the operations
 if z ==1:
  print(x+y)
 elif z == 2 :
